c3ce206f4d3f546b730edf2aeda328e55884264a
siva-geddada <siva.cs579@gmail.com>
2023-07-31 05:18:03 +0530

    Final preview for version 0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -8,9 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
       if (editor) {
         const selectedText = editor.document.getText(editor.selection);
         if (selectedText) {
+          const configuration = vscode.workspace.getConfiguration('extension');
+          const customText = configuration.get<string>('customText') ?? '✌️'; // Default to "✌️" if the setting is not defined
+
           editor.edit((editBuilder) => {
             const currentPosition = editor.selection.active;
-            const logStatement = `console.log(${selectedText});`;
+            const logStatement = `console.log('${customText}${selectedText} --->', ${selectedText});\n`;
             editBuilder.insert(
               currentPosition.with(currentPosition.line + 1, 0),
               logStatement
@@ -23,6 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 }
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
